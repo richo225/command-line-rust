44ae689568340086fb80918b04435fd019105d9f
Richard Bates <rich.bates@protonmail.com>
2023-05-12 23:32:03 +0100

    Refactor test functions to return custom type

--- echor/tests/main_test.rs
+++ echor/tests/main_test.rs
@@ -2,61 +2,76 @@ use assert_cmd::Command;
 use predicates::prelude::predicate;
 use std::fs;
 
+// Result<Command, CargoError>
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_with_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_with_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert().failure().stderr(predicate::str::contains(
         "The following required arguments were not provided:",
     ));
+
+    Ok(())
 }
 
 #[test]
-fn runs_with_arg() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn runs_with_arg() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("here is some test text").assert().success();
+
+    Ok(())
 }
 
 #[test]
 // echo "Hello there"
-fn hello1() {
-    let expected = fs::read_to_string("tests/expected/hello1.txt").unwrap();
+fn hello1() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
 
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+
+    Ok(())
 }
 
 #[test]
 // echo -n "Hello  there"
-fn hello1_n() {
-    let expected = fs::read_to_string("tests/expected/hello1.n.txt").unwrap();
+fn hello1_n() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello1.n.txt")?;
 
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.args(["-n", "Hello  there"])
         .assert()
         .success()
         .stdout(expected);
+
+    Ok(())
 }
 
 #[test]
 // echo "Hello"  "there"
-fn hello2() {
-    let expected = fs::read_to_string("tests/expected/hello2.txt").unwrap();
+fn hello2() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
 
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.args(["Hello", "there"])
         .assert()
         .success()
         .stdout(expected);
+
+    Ok(())
 }
 
 #[test]
 // echo -n "Hello"  "there"
-fn hello2_n() {
-    let expected = fs::read_to_string("tests/expected/hello2.n.txt").unwrap();
+fn hello2_n() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello2.n.txt")?;
 
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.args(["-n", "Hello", "there"])
         .assert()
         .success()
         .stdout(expected);
+
+    Ok(())
 }

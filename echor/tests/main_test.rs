4fe539b837e92fd2aebef16c8e231d3b35b8e074
Richard Bates <rich.bates@protonmail.com>
2023-05-12 23:43:11 +0100

    Refactor file and args into helper function

--- echor/tests/main_test.rs
+++ echor/tests/main_test.rs
@@ -5,6 +5,15 @@ use std::fs;
 // Result<Command, CargoError>
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
+fn run_command(expected_file: &str, args: &[&str]) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+
+    cmd.args(args).assert().success().stdout(expected);
+
+    Ok(())
+}
+
 #[test]
 fn dies_with_no_args() -> TestResult {
     let mut cmd = Command::cargo_bin("echor")?;
@@ -26,52 +35,23 @@ fn runs_with_arg() -> TestResult {
 #[test]
 // echo "Hello there"
 fn hello1() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
-
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-
-    Ok(())
+    run_command("tests/expected/hello1.txt", &["Hello", "there"])
 }
 
 #[test]
 // echo -n "Hello  there"
 fn hello1_n() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello1.n.txt")?;
-
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(["-n", "Hello  there"])
-        .assert()
-        .success()
-        .stdout(expected);
-
-    Ok(())
+    run_command("tests/expected/hello1.n.txt", &["-n", "Hello  there"])
 }
 
 #[test]
 // echo "Hello"  "there"
 fn hello2() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
-
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(["Hello", "there"])
-        .assert()
-        .success()
-        .stdout(expected);
-
-    Ok(())
+    run_command("tests/expected/hello2.txt", &["Hello", "there"])
 }
 
 #[test]
 // echo -n "Hello"  "there"
 fn hello2_n() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello2.n.txt")?;
-
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(["-n", "Hello", "there"])
-        .assert()
-        .success()
-        .stdout(expected);
-
-    Ok(())
+    run_command("tests/expected/hello2.n.txt", &["-n", "Hello", "there"])
 }

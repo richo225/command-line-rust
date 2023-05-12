867efd3863f2d6e51ccc84f8115059319b551bed
Richard Bates <rich.bates@protonmail.com>
2023-05-12 21:51:06 +0100

    Print input text to stdout depending on -n flag

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,28 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Richard Bates <rich.bates@protonmail.com>")
         .about("Rust implementation of echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input some text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Don't print new lines")
+                .takes_value(false),
+        )
         .get_matches();
+
+    let omit_new_line: bool = matches.is_present("omit_newline");
+    let ending: &str = if omit_new_line { "" } else { "\n" };
+
+    let text: Vec<String> = matches.values_of_lossy("text").unwrap();
+    println!("{:?}{}", text.join(", "), ending);
 }

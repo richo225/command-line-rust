379fa116d7fbe2f9c94a5e4a4f3548407a7d24be
Richard Bates <rich.bates@protonmail.com>
2023-05-13 12:53:41 +0100

    Initial tests for empty file scenario

--- catr/tests/main_test.rs
+++ catr/tests/main_test.rs
@@ -1,7 +1,17 @@
+use std::fs;
+
 use assert_cmd::Command;
 use predicates::prelude::predicate;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+fn run(expected_file: &str, args: &[&str]) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("catr")?;
+    cmd.args(args).assert().success().stdout(expected);
+
+    Ok(())
+}
 
 #[test]
 fn dies_with_no_args() -> TestResult {
@@ -11,4 +21,25 @@ fn dies_with_no_args() -> TestResult {
         .stderr(predicate::str::contains("<FILE_NAME>"));
 
     Ok(())
+}
+
+#[test]
+fn empty() -> TestResult {
+    run("tests/expected/empty.txt.out", &["tests/inputs/empty.txt"])
+}
+
+#[test]
+fn empty_b() -> TestResult {
+    run(
+        "tests/expected/empty.b.txt.out",
+        &["tests/inputs/empty.b.txt"],
+    )
+}
+
+#[test]
+fn empty_n() -> TestResult {
+    run(
+        "tests/expected/empty.n.txt.out",
+        &["tests/inputs/empty.n.txt"],
+    )
 }

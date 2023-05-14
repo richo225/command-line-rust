1dd6f43b8e1bda6babbf5866fae0ad8a80b7af46
Richard Bates <rich.bates@protonmail.com>
2023-05-14 21:32:10 +0100

    Parse file arguments and read file to stdout

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,7 +1,14 @@
 use clap::{App, Arg};
+use std::fs;
+
+#[derive(Debug)]
+struct Config {
+    number_all_lines: bool,
+    number_nonblank_lines: bool,
+    files: Vec<String>,
+}
 
 fn main() {
-    // cargo run -- file1 file2 file3
     let matches = App::new("catr")
         .version("0.1.0")
         .author("Richard Bates <rich.bates@protonmail.com")
@@ -13,7 +20,34 @@ fn main() {
                 .required(true)
                 .min_values(1),
         )
+        .arg(
+            Arg::with_name("number")
+                .long("--number")
+                .short("n")
+                .help("Number all output lines")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("number-nonblank")
+                .long("--number-nonblank")
+                .short("b")
+                .help("Number only non-blank lines")
+                .takes_value(false),
+        )
         .get_matches();
 
-    println!("{:#?}", matches)
+    let config = Config {
+        files: matches.values_of_lossy("file_name").unwrap(),
+        number_all_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number-nonblank"),
+    };
+
+    parse_file(&config);
+}
+
+fn parse_file(config: &Config) {
+    for file in &config.files {
+        let content = fs::read_to_string(file);
+        print!("{}", content.unwrap())
+    }
 }
